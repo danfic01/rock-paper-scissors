1a9c550d600202c72bc05a58b801bae75a1ab608
danfic01 <danielrfic@gmail.com>
2024-04-15 19:15:05 -0400

    Add Get_Computer_Selection function
    Javascript file updated with function to get computer selection or rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+// Declare a function to get computer selection
+function get_computer_selection() {
+    
+    // Generate a random integer between 0-2 and store the value in a variable
+    const randInt = Math.floor(Math.random()*3);
+    
+    // Determine computer selection
+    if (randInt === 0) {
+        //if 0 computer selection is rock
+        return "rock";
+    }
+    else if (randInt === 1) {
+        // else if 1 computer selection is paper
+        return "paper";
+    }
+    else {
+        // else (2) computer selection is scissors
+        return "scissors";
+    }
+}
+
+// test get_computer_selection() returns expected outcome
+console.log(get_computer_selection());
