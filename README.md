fd81a0e14913941ca3ffc015834ffbab28483539
danfic01 <danielrfic@gmail.com>
2024-04-15 19:52:45 -0400

    Add playRound function
    Javascript file updated with with function to play a round of RPS. Function names updated to meet JS convetions.

--- README.md
+++ README.md
@@ -7,18 +7,18 @@ Pseudocode
   - Generate a random integer between 0-2 and store the value in a variable
   - If 0 computer selection is rock, else if 1 computer selection is paper, else computer selection is scissors
 
-- Declare a function to play a round of RPS.
+- Declare a function to play a round of RPS
   - Get user input and store it in a variable
   - While user input is not rock, not paper and not scissors (case insensitive) get user input and reassign variable
   - Call function to get computer selection and store the return value in a variable
   - Check user input against computer selection to determine winner of round
   - Return round winner
 
-- Declare a function to run the game.
+- Declare a function to run the game
   - Declare a variable to track the winner and set its initial value to 0
   - For round in game do the following:
     - Call function to play round and store the return value in a variable
     - If the player wins increment winner by 1, else decrement winner by 1 when computer wins a round
-  - If winner variable is greater than 0 player wins, else computer wins. (This structure assumes an odd number of rounds, with an even number of rounds a tie would be possible; winner variable is equal to 0).
+  - If winner variable is greater than 0 player wins, else if less than 0 computer wins, else the game is a tie 
 
   - Call function to run the game
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Declare a function to get computer selection
-function get_computer_selection() {
+function getComputerSelection() {
     
     // Generate a random integer between 0-2 and store the value in a variable
     const randInt = Math.floor(Math.random()*3);
@@ -20,5 +20,68 @@ function get_computer_selection() {
     }
 }
 
-// test get_computer_selection() returns expected outcome
-console.log(get_computer_selection());
+// Declare a function to play a round of RPS
+function playRound() {
+    // Get user input and store it in a variable
+    let userInput = prompt('CHOOSE ONE: ROCK | PAPER | SCISSORS ').toLowerCase(); // store user input as lower case
+
+    // While user input is not rock, not paper and not scissors (case insensitive) get user input and reassign variable
+    while (userInput !== 'rock' && userInput !== 'paper' && userInput !== 'scissors') {
+        userInput = prompt('PLEASE ENTER ONLY ROCK PAPER OR SCISSORS').toLowerCase();
+    }
+
+    // Call function to get computer selection and store the return value in a variable
+    let computerSelection = getComputerSelection();
+    // test getComputerSelection() returns expected outcome
+    console.log('COMPUTER: ' + computerSelection);
+
+    // Check user input against computer selection to determine winner of round
+    switch (userInput){
+        
+        // define outcomes when user chooses rock
+        case 'rock':
+            if(computerSelection === 'scissors') {
+                return 'player';
+            }
+            else if (computerSelection === 'paper'){
+                return 'computer';
+            }
+            else {
+                return 'tie';
+            }
+            break;
+        
+        // define outcomes when user chooses paper
+        case 'paper':
+            if(computerSelection === 'rock') {
+                return 'player';
+            }
+            else if (computerSelection === 'scissors'){
+                return 'computer';
+            }
+            else {
+                return 'tie';
+            }
+            break;
+        
+        // define outcomes when user chooses scissors
+        case 'scissors':
+            if(computerSelection === 'paper') {
+                return 'player';
+            }
+            else if (computerSelection === 'rock'){
+                return 'computer';
+            }
+            else {
+                return 'tie';
+            }
+            break;
+        
+        default:
+            console.log('algorithmic error');
+        }
+}
+
+
+// test playRound() returns expected outcome
+console.log(playRound());
